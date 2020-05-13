478f30988f91787c14a49e3487a8b63135b0b954
Bobby Mantoni <bobby@mantoni.me>
2020-05-13 23:09:52 +0100

    tune the attractor lockon/lock-off

--- lib/zombie_game.dart
+++ lib/zombie_game.dart
@@ -84,145 +84,156 @@ enum Direction { Left, Right, Up, Down }
 
 class Zombie extends GridSprite {
   // to tune the starting positions
-  static const int SPAWN_HEIGHT_RANGE = 5;
+  static const int SPAWN_HEIGHT_RANGE      = 5;
   static const int SPAWN_RIGHT_MARGIN_COLS = 3;
 
   // how frequently to move (higher:more often)
-  static const int MOVE_FREQ = 5;
+  static const int MOVE_FREQ               = 5;
 
   // how frequently to animate sprite (lower:slower)
-  static const double ANIMATE_FREQ = 0.3;
+  static const double ANIMATE_FREQ         = 0.3;
 
   // fixed based on sprite sheet
-  static const double TEXTURE_SIZE = 32.0;
-  static const double ZOMBIE_SIZE = TEXTURE_SIZE * 3;
+  static const double TEXTURE_SIZE         = 32.0;
+  static const double ZOMBIE_SIZE          = TEXTURE_SIZE * 3;
+
+  static const double LOCKON_COOLDOWN      = 0.15;
+  static const int RETHINK_MOVE_FREQ       = 2;
+  static const int MAX_LOCKON_DIST         = 20;
 
   MovementMode _mode = MovementMode.Random;
-  // randomly switch mode back to random
   double _rethinkMovementModeEveryTics = 0;
   double _timeSinceRethoughtMovement = 0;
-  static const int RETHINK_MOVE_FREQ = 4;
-  static const int MIN_LOCKON_DIST = 20;
+  double _lockonCooldownRemaining = 0;
+  double _timeSinceMove = 0;
 
   Randomiser _r = Randomiser();
   double _moveEveryTics = 0;
   AnimationComponent _component;
   get component => _component;
 
-  double _timeSinceMove = 0;
-
   Zombie(PlayField field) : super(field) {
     setRandomMoveDelay();
     setRandomRethinkDelay();
-    
-        _component = AnimationComponent.sequenced(
-            ZOMBIE_SIZE, ZOMBIE_SIZE, 'zombie-v3-sheet.png', 3,
-            textureWidth: TEXTURE_SIZE,
-            textureHeight: TEXTURE_SIZE,
-            stepTime: ANIMATE_FREQ);
-        setStartPosition();
-      }
-    
-      void setStartPosition() {
-        loc = GridPoint(_r.getRand(1, field.numCols - SPAWN_RIGHT_MARGIN_COLS),
-            _r.getRand(1, SPAWN_HEIGHT_RANGE));
-        updateComponentPosition();
-      }
-    
-      void setRandomMoveDelay() {
-        _moveEveryTics = _r.getRand(1, MOVE_FREQ) / 2;
-        _timeSinceMove = 0;
-      }
-    
-      void move(double timeSince) {
-        _timeSinceMove += timeSince;
-        if (_timeSinceMove <= _moveEveryTics) return;
-    
-        _timeSinceRethoughtMovement += timeSince;
-        _rethinkMovementMode();
-    
-        switch (_mode) {
-          case MovementMode.Random:
-            _moveRandomly();
-            setRandomMoveDelay();
-            break;
-          case MovementMode.Attracted:
-            _moveTowardsAttractor();
-            setRandomMoveDelay(); // and speed it up, since we're chasing something
-            break;
-          default:
-        }
-      }
-    
-      void _rethinkMovementMode() {
-        lockonIfSomethingIsClose();
-        randomlyGoBackToRandom();
-      }
 
-      void setRandomRethinkDelay() {
-        _rethinkMovementModeEveryTics = _r.getRand(1, RETHINK_MOVE_FREQ) / 2;
+    _component = AnimationComponent.sequenced(
+        ZOMBIE_SIZE, ZOMBIE_SIZE, 'zombie-v3-sheet.png', 3,
+        textureWidth: TEXTURE_SIZE,
+        textureHeight: TEXTURE_SIZE,
+        stepTime: ANIMATE_FREQ);
+    setStartPosition();
+  }
+
+  void setStartPosition() {
+    loc = GridPoint(_r.getRand(1, field.numCols - SPAWN_RIGHT_MARGIN_COLS),
+        _r.getRand(1, SPAWN_HEIGHT_RANGE));
+    updateComponentPosition();
+  }
+
+  void setRandomMoveDelay() {
+    _moveEveryTics = _r.getRand(1, MOVE_FREQ) / 2;
+    _timeSinceMove = 0;
+  }
+
+  void move(double timeSince) {
+    _timeSinceMove += timeSince;
+    if (_timeSinceMove <= _moveEveryTics) return;
+
+    _lockonCooldownRemaining =
+        _lockonCooldownRemaining > 0 ? _lockonCooldownRemaining - timeSince : 0;
+
+    _timeSinceRethoughtMovement += timeSince;
+    _rethinkMovementMode();
+
+    switch (_mode) {
+      case MovementMode.Random:
+        _moveRandomly();
+        setRandomMoveDelay();
+        break;
+      case MovementMode.Attracted:
+        _moveTowardsAttractor();
+        setRandomMoveDelay(); // and speed it up, since we're chasing something
+        break;
+      default:
+    }
+  }
+
+  void _rethinkMovementMode() {
+    switch (_mode) {
+      case MovementMode.Random:
+        lockonIfSomethingIsClose();
+        break;
+      case MovementMode.Attracted:
+        randomlyGoBackToRandom();
+        break;
+    }
+  }
+
+  void setRandomRethinkDelay() {
+    _rethinkMovementModeEveryTics = _r.getRand(1, RETHINK_MOVE_FREQ) / 12;
+  }
+
+  void randomlyGoBackToRandom() {
+    if (_timeSinceRethoughtMovement > _rethinkMovementModeEveryTics) {
+      _timeSinceRethoughtMovement = 0;      
+      _r.doRandomly(1, 2, () {
+        _mode = MovementMode.Random;
+        setRandomRethinkDelay();
+        _lockonCooldownRemaining = LOCKON_COOLDOWN;
+        print("Switched to random");
+      });
+    }
+  }
+
+  void lockonIfSomethingIsClose() {
+    if (_lockonCooldownRemaining <= 0 && field.getAttractors().length > 0) {
+      final nearest = _getNearestAttractor();
+      if (nearest.getWeightedDistance(this) < MAX_LOCKON_DIST) {
+        _mode = MovementMode.Attracted;
+        print("Locked on");
       }
-    
-      void randomlyGoBackToRandom() {
-        if (_timeSinceRethoughtMovement > _rethinkMovementModeEveryTics) {
-          _timeSinceRethoughtMovement = 0;
-          if (_mode == MovementMode.Attracted) {
-            _r.doRandomly(1, 3, () { 
-              _mode = MovementMode.Random;
-              setRandomRethinkDelay();
-              // TODO set a cooldown, or it will just go back to attracted
-            });
-          }
-        }
-      }
-    
-      void lockonIfSomethingIsClose() {
-        if (field.getAttractors().length > 0) {
-          final nearest = _getNearestAttractor();
-          if (nearest.getWeightedDistance(this) < MIN_LOCKON_DIST) {
-            _mode = MovementMode.Attracted;
-          }
-        }
-      }
-    
-      void _moveTowardsAttractor() {
-        // find nearest & most attractive attractor
-        //field.min
-        //   look in an expanding square around self
-        // move towards it
-        if (field.getAttractors().length > 0) {
-          final a = _getNearestAttractor();
-          _moveTowards(a);
-        }
-      }
-    
-      Attractor _getNearestAttractor() {
-        return field.getAttractors().reduce((value, element) => 
-          value.getWeightedDistance(this) < element.getWeightedDistance(this) ? 
-          value : element);
-      }
-    
-      void _moveTowards(Attractor a) {
-        if (a.loc.x < loc.x) _move(Direction.Left, 1);
-        if (a.loc.x > loc.x) _move(Direction.Right, 1);
-        if (a.loc.y < loc.y) _move(Direction.Up, 1);
-        if (a.loc.y > loc.y) _move(Direction.Down, 1);
-      }
-    
-      void _moveRandomly() {
-        var dist = _r.getRand(1, 5); // 20% change of moving 2
-        _move(Direction.values[_r.getRand(0, 3)], dist == 1 ? 2 : 1);
-      }
-    
-      void _move(Direction direction, int distance) {
-        translate(direction, distance: distance);
-        updateComponentPosition();
-      }
-    
-      void updateComponentPosition() {
-        _component.x = locationPoint.x;
-        _component.y = locationPoint.y;
-      }
+    }
+  }
+
+  void _moveTowardsAttractor() {
+    // find nearest & most attractive attractor
+    //field.min
+    //   look in an expanding square around self
+    // move towards it
+    if (field.getAttractors().length > 0) {
+      final a = _getNearestAttractor();
+      _moveTowards(a);
+    }
+  }
+
+  Attractor _getNearestAttractor() {
+    return field.getAttractors().reduce((value, element) =>
+        value.getWeightedDistance(this) < element.getWeightedDistance(this)
+            ? value
+            : element);
+  }
+
+  void _moveTowards(Attractor a) {
+    if (a.loc.x < loc.x) _move(Direction.Left, 1);
+    if (a.loc.x > loc.x) _move(Direction.Right, 1);
+    if (a.loc.y < loc.y) _move(Direction.Up, 1);
+    if (a.loc.y > loc.y) _move(Direction.Down, 1);
+  }
+
+  void _moveRandomly() {
+    var dist = _r.getRand(1, 5); // 20% change of moving 2
+    _move(Direction.values[_r.getRand(0, 3)], dist == 1 ? 2 : 1);
+  }
+
+  void _move(Direction direction, int distance) {
+    translate(direction, distance: distance);
+    updateComponentPosition();
+  }
+
+  void updateComponentPosition() {
+    _component.x = locationPoint.x;
+    _component.y = locationPoint.y;
+  }
 }
 
 class ZombieGame extends BaseGame with TapDetector {
